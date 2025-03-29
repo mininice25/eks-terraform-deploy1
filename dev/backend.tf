413c8fefe5e57bef620b7aedaee0848965b013d6
chidi <mininice252gmail.com>
2025-03-29 04:30:53 -0600

    enhanced infra deployment

--- dev/backend.tf
+++ dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "prod/terraform.state"
-    bucket         = "mystoragebucket-state-store-terraform-backend"
+    bucket         = "class38terra-terraform-backend"
     region         = "us-west-2"
     dynamodb_table = "terraform-state-locking"
   }
