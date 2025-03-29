3c27418d2b892f6bc727cb8fe6840e8e0216c401
chidi <mininice252gmail.com>
2025-03-29 05:14:06 -0600

    bucket name

--- prod/backend.tf
+++ prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "prod/terraform.state"
-    bucket         = "dominionclass39-state-store-terraform-backend"
+    bucket         = "dominionclass38b-state-store-terraform-backend"
     region         = "us-west-2"
     dynamodb_table = "terraform-state-locking"
   }
